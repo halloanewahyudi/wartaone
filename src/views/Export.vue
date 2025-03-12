947b9d05be024857cae83dd7d97e82a934fb802f
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2025-03-12 08:09:00 +0700

    kembali ke entries

--- src/views/Export.vue
+++ src/views/Export.vue
@@ -5,7 +5,7 @@
             <h1 class="text-xl font-bold mb-4">Export Data Pemudik</h1>
             <div class="flex gap-4 flex-wrap items-center justify-center">
                 <ExportData />
-                <router-link to="/entries" class="btn">🏠 Kembali Entries</router-link>
+                <router-link to="/entries" class="btn">🏠 Kembali ke entries</router-link>
             </div>
         </div>
 
